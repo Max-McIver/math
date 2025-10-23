2008d310a760fc0aca3827919273d1c372e4ef07
Max-McIver <Max-McIver@yandex.com>
2025-10-23 21:26:21 +0300

    Added godoc comments

--- math.go
+++ math.go
@@ -1,5 +1,11 @@
+// Package math provides basic mathematical operations.
+// This package includes functions for addition and other arithmetic operations.
 package math
 
+// Add takes two integers and returns their sum.
+// For more information on addition, visit [mathsisfun]
+//
+// [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
